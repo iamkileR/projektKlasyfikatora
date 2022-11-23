f7b59b5aa77a2261d35599419437faadb3151405
iamkileR <jakubsadza@gmail.com>
2022-11-23 10:30:27 +0100

    Basic Emotion Classifaction App

---
+++ main.py
@@ -0,0 +1,134 @@
+import requests
+import tensorflow as tf
+import os
+import cv2
+import imghdr
+import numpy as np
+from PIL.Image import Image
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from keras.metrics import Precision, Recall, BinaryAccuracy
+from keras.models import load_model
+from matplotlib import pyplot as plt
+
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+print(tf.config.list_physical_devices('GPU'))
+
+#Remove dodgy images
+data_dir = 'data'
+image_exts = ['jpeg','jpg', 'bmp', 'png']
+for image_class in os.listdir(data_dir):
+    for image in os.listdir(os.path.join(data_dir, image_class)):
+        image_path = os.path.join(data_dir, image_class, image)
+        try:
+            img = cv2.imread(image_path)
+            tip = imghdr.what(image_path)
+            if tip not in image_exts:
+                print('Image not in ext list {}'.format(image_path))
+                os.remove(image_path)
+        except Exception as e:
+            print('Issue with image {}'.format(image_path))
+            # os.remove(image_path)
+
+#Load Data
+data = tf.keras.utils.image_dataset_from_directory('data')
+data_iterator = data.as_numpy_iterator()
+batch = data_iterator.next()
+fig, ax = plt.subplots(ncols=4, figsize=(20,20))
+
+for idx, img in enumerate(batch[0][:4]):
+    ax[idx].imshow(img.astype(int))
+    ax[idx].title.set_text(batch[1][idx])
+
+#Scale Data
+data = data.map(lambda x,y: (x/255, y))
+data.as_numpy_iterator().next()
+
+#Split Data
+train_size = int(len(data)*.7)
+val_size = int(len(data)*.2)
+test_size = int(len(data)*.1)
+print(train_size)
+
+train = data.take(train_size)
+val = data.skip(train_size).take(val_size)
+test = data.skip(train_size+val_size).take(test_size)
+
+#Build Deep Learning Model
+print(train)
+
+model = Sequential()
+model.add(Conv2D(16, (3,3), 1, activation='relu', input_shape=(256,256,3)))
+model.add(MaxPooling2D())
+model.add(Conv2D(32, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(16, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
+
+print(model.summary())
+
+#Train
+logdir='logs'
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+hist = model.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback])
+
+#Plot Performance
+fig = plt.figure()
+plt.plot(hist.history['loss'], color='teal', label='loss')
+plt.plot(hist.history['val_loss'], color='orange', label='val_loss')
+fig.suptitle('Loss', fontsize=20)
+plt.legend(loc="upper left")
+plt.show()
+
+fig = plt.figure()
+plt.plot(hist.history['accuracy'], color='teal', label='accuracy')
+plt.plot(hist.history['val_accuracy'], color='orange', label='val_accuracy')
+fig.suptitle('Accuracy', fontsize=20)
+plt.legend(loc="upper left")
+plt.show()
+
+#Evaluate
+pre = Precision()
+re = Recall()
+acc = BinaryAccuracy()
+
+for batch in test.as_numpy_iterator():
+    X, y = batch
+    yhat = model.predict(X)
+    pre.update_state(y, yhat)
+    re.update_state(y, yhat)
+    acc.update_state(y, yhat)
+
+print(pre.result(), re.result(), acc.result())
+
+#Test
+img = cv2.imread('usmiech.jpg')
+plt.imshow(img)
+plt.show()
+
+resize = tf.image.resize(img, (256,256))
+plt.imshow(resize.numpy().astype(int))
+plt.show()
+
+yhat = model.predict(np.expand_dims(resize/255, 0))
+print(yhat)
+
+if yhat > 0.5:
+    print(f'Rozpoznano emocje - Smutek')
+else:
+    print(f'Rozpoznano emocje - Radosny')
+
+#Save Model
+model.save(os.path.join('models','imageclassifier.h5'))
+new_model = load_model('models/imageclassifier.h5')
+new_model.predict(np.expand_dims(resize/255, 0))
+
